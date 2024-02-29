3cb1751cbcdf024618f629b684946322953ae2ea
BishnuPandey <pandeybishnupd@gmail.com>
2024-02-29 21:29:00 +0545

    fix: rubocop offence

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,10 +12,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.from_omniauth(auth)
     if user.present?
       sign_out_all_scopes
-      flash[:success] = t "devise.omniauth_callback.success", kind: "Google"
+      flash[:success] = t 'devise.omniauth_callback.success', kind: 'Google'
       sign_in_and_redirect user, event: :authentication
     else
-      flash[:alert] = t "devise.omniauth_callback.failure", kind: "google", reason: "#{auth.info.email} is not authorized"
+      flash[:alert] =
+        t 'devise.omniauth_callback.failure', kind: 'google', reason: "#{auth.info.email} is not authorized"
       redirect_to new_user_session_path
     end
   end
@@ -43,6 +44,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def auth
-    @auth ||= request.env["omniauth.auth"]
+    @auth ||= request.env['omniauth.auth']
   end
 end
